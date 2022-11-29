0d336a5fb0c4209606dc2f3310702003201fd55a
John Boone <j.raditya.b@gmail.com>
2022-11-29 13:59:20 +0800

    seed work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying all the games!!"
+Games.destroy_all
+
+puts "Creating 20 random games!"
+puts "loading games..."
+20.times do
+  Game.create(
+    name: Faker::Game.title,
+    description: Faker::Game.genre,
+    platform: Faker::Game.platform,
+    picture_url: "https://loremflickr.com/320/240/console",
+    price_per_day: rand(1..10).sample
+  )
+end
+puts "#{games.count} games loaded."
