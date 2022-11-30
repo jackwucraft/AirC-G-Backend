078e805c19d13656cd11f19a6091a73dfa56bd2d
Jack Wu <jackwuchn@outlook.com>
2022-11-30 14:45:13 +0800

    platform picturn

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,17 @@ Game.destroy_all
 puts "Creating 20 random games!"
 puts "loading games..."
 20.times do
+  platformeg = [
+    { platform: "xbox", picture_url: "https://images.unsplash.com/photo-1605901309584-818e25960a8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3119&q=80"},
+    { platform: "ps5", picture_url: "https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"},
+    { platform: "switch", picture_url: "https://images.unsplash.com/photo-1585427795543-33cf23ea2853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"}
+  ].sample
+  name = Faker::Game.title
   Game.create(
-    name: Faker::Game.title,
+    name: name,
     description: "#{name} is a 2017 action-adventure game developed and published by Nintendo for the Nintendo Switch and Wii U consoles. The game is an installment of The Legend of Zelda series and is set at the end of its timeline. The player controls an amnesiac Link, who awakens from a hundred-year slumber, and attempts to regain his memories and prevent the destruction of Hyrule by Calamity Ganon.",
-    platform: ["xbox", "ps5", "switch"].sample,
-    picture_url: "https://loremflickr.com/320/240/console",
+    platform: platformeg[:platform],
+    picture_url: platformeg[:picture_url],
     price_per_day: rand(1..10)
   )
 end
